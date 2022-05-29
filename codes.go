2af8648424c2767e70bd31ea7670adfe35d87082
Charles Iliya Krempeaux <iamreiver@gmail.com>
2022-05-28 22:53:45 -0700

    initial commits

---
+++ codes.go
@@ -0,0 +1,66 @@
+package sgr
+
+import (
+	"github.com/reiver/go-sgr/cs"
+)
+
+const (
+	Off                    Code = cs.SGR_0
+	WeightBold             Code = cs.SGR_1
+	WeightFaint            Code = cs.SGR_2
+	StyleItalic            Code = cs.SGR_3
+	UnderlineSingle        Code = cs.SGR_4
+	BlinkSlow              Code = cs.SGR_5
+	BlinkRapid             Code = cs.SGR_6
+	ReverseColor           Code = cs.SGR_7
+	Conceal                Code = cs.SGR_8
+	StrikeOut              Code = cs.SGR_9
+	Font1                  Code = cs.SGR_10
+	Font2                  Code = cs.SGR_11
+	Font3                  Code = cs.SGR_12
+	Font4                  Code = cs.SGR_13
+	Font5                  Code = cs.SGR_14
+	Font6                  Code = cs.SGR_15
+	Font7                  Code = cs.SGR_16
+	Font8                  Code = cs.SGR_17
+	Font9                  Code = cs.SGR_18
+	Font10                 Code = cs.SGR_19
+	StyleFraktur           Code = cs.SGR_20
+	UnderlineDouble        Code = cs.SGR_21
+	WeightOff              Code = cs.SGR_22
+	StyleOff               Code = cs.SGR_23
+	UnderlineOff           Code = cs.SGR_24
+	BlinkOff               Code = cs.SGR_25
+	ProportionalSpacing    Code = cs.SGR_26
+	ReverseColorOff        Code = cs.SGR_27
+	ConcealOff             Code = cs.SGR_28
+	StrikeOutOff           Code = cs.SGR_29
+	ForegroundColorBlack   Code = cs.SGR_30
+	ForegroundColorRed     Code = cs.SGR_31
+	ForegroundColorGreen   Code = cs.SGR_32
+	ForegroundColorYellow  Code = cs.SGR_33
+	ForegroundColorBlue    Code = cs.SGR_34
+	ForegroundColorMagenta Code = cs.SGR_35
+	ForegroundColorCyan    Code = cs.SGR_36
+	ForegroundColorWhite   Code = cs.SGR_37
+
+	ForegroundColorDefault Code = cs.SGR_39
+	BackgroundColorBlack   Code = cs.SGR_40
+	BackgroundColorRed     Code = cs.SGR_41
+	BackgroundColorGreen   Code = cs.SGR_42
+	BackgroundColorYellow  Code = cs.SGR_43
+	BackgroundColorBlue    Code = cs.SGR_44
+	BackgroundColorMagenta Code = cs.SGR_45
+	BackgroundColorCyan    Code = cs.SGR_46
+	BackgroundColorWhite   Code = cs.SGR_47
+
+	BackgroundColorDefault Code = cs.SGR_49
+	ProportionalSpacingOff Code = cs.SGR_50
+
+	Overline               Code = cs.SGR_53
+
+	OverlineOff            Code = cs.SGR_55
+
+	Superscript            Code = cs.SGR_73
+	Subscript              Code = cs.SGR_74
+)
